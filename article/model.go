1bcfe87b23f683a7819a01d6477b5404d811aba5
YoonBaek <royal729@naver.com>
2021-12-30 23:53:25 +0900

    fear: [Model] Add json format backticks

--- article/model.go
+++ article/model.go
@@ -4,11 +4,11 @@ package article
 // belongs to: 종목
 // 추후 추가 기능: weather
 type Article struct {
-	PostNo      uint   `gorm:"primaryKey"`
-	CodeNo      string //연동 게시판 추후 foreign key 적용 예정
-	Title       string
-	Content     string
-	ReportedCnt uint
-	CreatedAt   int64 `gorm:"autoCreateTime"`
-	UpdatedAt   int64 `gorm:"autoUpdateTime"`
+	PostNo      uint   `gorm:"primaryKey" json:"post_no"`
+	CodeNo      string `json:"code_no"` //연동 게시판 추후 foreign key 적용 예정
+	Title       string `json:"title"`
+	Content     string `json:"content"`
+	ReportedCnt uint   `json:"reported_cnt"`
+	CreatedAt   int64  `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   int64  `gorm:"autoUpdateTime" json:"updated_at"`
 }
