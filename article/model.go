6b5c5e12d3e36e191b6139a8da46b789b1bacc45
YoonBaek <royal729@naver.com>
2021-12-30 19:13:41 +0900

    feat: [MODEL] Add Article model

--- article/model.go
+++ article/model.go
@@ -1,9 +1,11 @@
 package article
 
 // article 모델
+// belongs to: 종목
+// 추후 추가 기능: weather
 type Article struct {
 	PostNo      uint   `gorm:"primaryKey"`
-	CodeNo      string //연동 게시판
+	CodeNo      string //연동 게시판 추후 foreign key 적용 예정
 	Title       string
 	Content     string
 	ReportedCnt uint
