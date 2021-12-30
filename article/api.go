0b37e05e15ec543381073b60940b216ef1b15c32
YoonBaek <royal729@naver.com>
2021-12-30 23:52:22 +0900

    feat: [VIEW] Add article.read

--- article/api.go
+++ article/api.go
@@ -1,23 +1,32 @@
 package article
 
 import (
+	dataBase "github.com/YoonBaek/ururu-server/db"
+
 	"github.com/gofiber/fiber/v2"
 )
-
-func Routes(app *fiber.App) {
-	app.Post("/", create)
-}
 
 // test
 // 1. 인스턴스 생성
 // 2. value 하드코딩
 // 3. post to DB
 func create(c *fiber.Ctx) error {
+	db := dataBase.DB()
 	atc := &Article{}
 	atc.Title = "첫 게시글 입니다."
 	atc.Content = "첫 게시글 내용 입니다."
 	atc.CodeNo = "BTC"
-
+	db.Create(atc)
 	err := c.JSON(&atc)
+	return err
+}
+
+func read(c *fiber.Ctx) error {
+	// id, _ := strconv.Atoi(c.Params("post_no"))
+	postNo := c.Params("post_no")
+	db := dataBase.DB()
+	article := &Article{}
+	db.Find(article, "post_no = ?", postNo)
+	err := c.JSON(*article)
 	return err
 }
