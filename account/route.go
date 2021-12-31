1e06c60ff8caef78ce6b1a575ab6d144187d4441
YoonBaek <royal729@naver.com>
2021-12-31 20:00:04 +0900

    style: Rename signin to signup

---
+++ account/route.go
@@ -0,0 +1,15 @@
+package account
+
+import "github.com/gofiber/fiber/v2"
+
+const appName URL = "/account"
+
+type URL string
+
+func (u URL) getURL(path string) string {
+	return string(u) + path
+}
+
+func Routes(app *fiber.App) {
+	app.Post(appName.getURL("/signup"), signup)
+}
