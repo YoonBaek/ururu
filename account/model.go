88b721af3edf52381b1690d5412b2858d9edbd9d
YoonBaek <royal729@naver.com>
2021-12-31 19:58:53 +0900

    feat: [Model] Add userSignIn form model

--- account/model.go
+++ account/model.go
@@ -2,13 +2,31 @@ package account
 
 type User struct {
 	UserId     uint   `gorm:"primaryKey" json:"user_id"`
-	Email      string `gorm:"unique;not null"`
-	password   string
-	IsVerified bool  `json:"is_verified"`
-	IsGuru     bool  `json:"is_guru"`
-	IsBlocked  bool  `json:"is_blocked"`
-	Starred    uint  `json:"starred"`
-	Starring   uint  `json:"starring"`
-	JoinedAt   int64 `gorm:"autoCreateTime" json:"joined_at"`
-	LastLogin  int64 `gorm:"autoUpdateTime" json:"last_login"`
+	Email      string `gorm:"unique;not null" json:"email"`
+	Password   string `gorm:"not null" json:"password"`
+	IsVerified bool   `json:"is_verified"`
+	IsGuru     bool   `json:"is_guru"`
+	IsBlocked  bool   `json:"is_blocked"`
+	Starred    uint   `json:"starred"`
+	Starring   uint   `json:"starring"`
+	JoinedAt   int64  `gorm:"autoCreateTime" json:"joined_at"`
+	LastLogin  int64  `gorm:"autoUpdateTime" json:"last_login"`
+}
+
+// 로그인 폼을 따로 받는 이우는....
+// 1. DB에는 암호화된 PW를 집어 넣기 위함
+// 2. 이메일 인증 절차를 거치기 위해서 (추후 구현)
+// 싱글톤패턴을 쓴 이유는 별건 없고 디폴트 값 설정을 위해서...
+// 추후 이메일 검증 로직을 추가할 예정
+type userSignInModel struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Repeat   string `json:"repeat"`
+	// IsEmailVerified bool   `json:"is_email_verified"` // tmp
+}
+
+func UserSignInModel() *userSignInModel {
+	u := &userSignInModel{}
+	// u.IsEmailVerified = true
+	return u
 }
